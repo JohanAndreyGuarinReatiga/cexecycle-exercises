e21ce7502baf059fc5b0c7f21b700014e27e65aa
JohanAndreyGuarinReatiga <186079123+JohanAndreyGuarinReatiga@users.noreply.github.com.>
2024-10-31 22:29:37 -0500

    Tiempo de viaje¶

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-#Divisores¶
-#Escriba un programa que entregue todos los divisores del número entero ingresado:
+#Tiempo de viaje¶
+#Un viajero desea saber cuánto tiempo tomó un viaje que realizó. Él tiene la duración en minutos de cada uno de los tramos del viaje.
 
-number = int(input("Enter a number: "))
-divisors = []
-for div in range(1, number + 1):
-    if number % div == 0: 
-        divisors.append(div)
-        
-print("Divisors:", " ".join(map(str, divisors)))
+#Desarrolle un programa que permita ingresar los tiempos de viaje de los tramos y entregue como resultado el tiempo total de viaje en formato horas:minutos.
+
+#El programa deja de pedir tiempos de viaje cuando se ingresa un 0.
+
+totalMin = 0
+
+while True:
+    duration = int(input("Duration (in minutes, enter 0 to finish): "))
+    if duration == 0:  
+        break
+    totalMin += duration  
+
+hours = totalMin // 60
+minutes = totalMin % 60
+
+print(f"Total travel time: {hours}:{minutes:02d} hours")
