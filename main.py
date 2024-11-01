e7eff693db49755cdb3aeeafed71b339603d2e70
JohanAndreyGuarinReatiga <186079123+JohanAndreyGuarinReatiga@users.noreply.github.com.>
2024-11-01 13:31:45 -0500

    ejercicio pi

--- main.py
+++ main.py
@@ -1,36 +1,10 @@
-#Dibujos de asteriscos
+#π
+#Desarolle un programa para estimar el valor de π usando la siguiente suma infinita
 
+n = int(input("Enter the number: "))
 
-print("Select the shape you want to draw:")
-print("1. Rectangle")
-print("2. Triangle")
-print("3. Hexagon")
-choice = int(input("Enter the number of your choice: "))
-if choice == 1:
-    height = int(input("Enter the height of the rectangle: "))
-    width = int(input("Enter the width of the rectangle: "))
-    
-    print("\nRectangle:")
-    for nnn1 in range(height):
-        print("*" * width)
-
-elif choice == 2:
-    height = int(input("Enter the height of the triangle: "))
-    
-    print("\nTriangle:")
-    for lineNum in range(1, height + 1):
-        print("*" * lineNum)
-
-elif choice == 3:
-    sideLength = int(input("Enter the side length of the hexagon: "))
-    
-    print("\nHexagon:")
-    for lineNum in range(1, sideLength + 1):
-        print(" " * (sideLength - lineNum) + "*" * (sideLength + 2 * (lineNum - 1)))
-    
-    for lineNum in range(sideLength - 1, 0, -1):
-        print(" " * (sideLength - lineNum) + "*" * (sideLength + 2 * (lineNum - 1)))
-
-else:
-    print("Invalid option. Please choose between 1, 2, and 3.")
-
+piNum = 0
+for term in range(n):
+    piNum += (-1) ** term / (2 * term + 1)
+piNum *= 4
+print(f"The approximation of pi with {n} terms is: {piNum}")
