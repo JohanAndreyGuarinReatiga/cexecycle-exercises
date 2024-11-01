0fd41f37af8c49b56e46de36ea5d3408c7f09b1f
JohanAndreyGuarinReatiga <186079123+JohanAndreyGuarinReatiga@users.noreply.github.com.>
2024-10-31 21:59:09 -0500

    Potencias de dos

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# Múltiplos
-#Escriba un programa que muestre la tabla de multiplicar del 1 al 10 del número ingresado por el usuario:
-
+#Potencias de dos
+#Escriba un programa que genere todas las potencias de 2, desde la 0-ésima hasta la ingresada por el usuario:
+import math
 
 while True:
-    number = int(input("Type your number: "))
-    for multiple in range(1, 11):
-        print(f"""
-      {number} * {multiple} = {number * multiple}
-      """)
-    confirmation = input("Do you want to continue? Type 'yes' to continue or 'no' to end the program: ")
-    if confirmation == 'no':
-        print("Program ended.")
-        break
-    elif confirmation == 'yes':
-        continue
+    quantity = int(input("""
+ Enter the maximum exponent for powers of 2:  """))
+    
+    if quantity >= 0:
+        for exponent in range(quantity + 1):
+            print(int(math.pow(2, exponent)), end=" ")
